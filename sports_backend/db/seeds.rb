f0a0fb3be29e418cacaa4690edd3689d6a3c943c
wwhite11 <2winstonrwhite@gmail.com>
2020-06-04 14:40:50 -0400

    scaffold data

--- sports_backend/db/seeds.rb
+++ sports_backend/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{name: Faker::Name.name,  email: Faker::Internet.email}])v
